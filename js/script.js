437fadbe25fd97a2236fd092172bcd4aca997e2b
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-10-07 14:59:49 +0200

    add code in js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-let nomeUtente = 'ciao'
-console.log (nomeUtente)
+let nomeUtente = prompt("Ciao e benvenuto nel mio sito, qual'è il tuo nome?")
+console.log(nomeUtente)
+
+let cognomeUtente = prompt("Qual'è il tuo cognome?")
+console.log(cognomeUtente)
+
+let colorePreferito = prompt("Ed ora puoi scrivermi il tuo colore preferito?")
+console.log(colorePreferito)
+
+alert(`Grazie per aver scelto Boolean; ${nomeUtente} ${cognomeUtente} ${colorePreferito} 21.
+Premere OK per continuare`)
